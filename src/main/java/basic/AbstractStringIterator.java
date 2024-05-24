641424a40718707fa766b5147f20c5013358b9d8
Igor Korolev <igorchelos@gmail.com>
2024-05-24 20:30:37 +0300

    1

--- src/main/java/basic/AbstractStringIterator.java
+++ src/main/java/basic/AbstractStringIterator.java
@@ -1,2 +1,21 @@
-package basic;public interface AbstractStringIterator {
+package basic;
+
+/**
+ * Абстрактное представление об итераторе для строки,
+ * содержащей математическое выражение.
+ */
+public interface AbstractStringIterator {
+    /**
+     * Проверяет наличие следующего элемента сроки.
+     *
+     * @return true, если следующий элемент строки существует, иначе - false.
+     */
+    boolean hasNext();
+
+    /**
+     * Возвращает следующий элемент строки.
+     *
+     * @return элемент строки.
+     */
+    String next();
 }
